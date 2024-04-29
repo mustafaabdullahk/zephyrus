8fc2ae371085513d6d81dd1a4b91dbd78ad88264
Mustafa Abdullah Kus <mustafa.kus@sparsetechnology.com>
2024-04-29 22:27:49 +0300

    add prometheus gauge impl test suite

---
+++ tests/pm_gauge_test.c
@@ -0,0 +1,22 @@
+#include "prometheus/pm_gauge.h"
+
+#include <stdio.h>
+#include <assert.h>
+
+void test_gauge_creation()
+{
+	// to do: gauge creation test
+}
+
+void test_gauge_set()
+{
+	// to do: gauge set test
+}
+
+int main()
+{
+	test_gauge_creation();
+	test_gauge_set();
+	printf("pm_gauge tests passed successfully!\n");
+	return 0;
+}
